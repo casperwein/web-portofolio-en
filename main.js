a02e1894d96639e6cafb92d4883f2fef7db3d409
casperWein <19201040.casperweinberger@gmail.com>
2022-07-02 20:22:41 +0700

    update

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+// burger events
 var hamburger = document.querySelector(".hamburger")
 hamburger.onclick = function() {
     var navBar = document.querySelector(".nav-bar")
     navBar.classList.toggle("active")
 }
 
+// form registrations
 var submit = document.querySelector(".button");
 submit.onclick = function formValidation() {
     var name = document.forms["message_form"]["name"].value;
@@ -15,10 +17,12 @@ submit.onclick = function formValidation() {
         alert("Nama harus diisi")
         return false
     }
+
     if (email == "") {
         alert("Email harus diisi")
         return false
     }
+
     if (phoneNumber == "") {
         alert("Nomor Handphone harus diisi")
         return false
@@ -30,6 +34,39 @@ submit.onclick = function formValidation() {
     }
 
     if (name != "" && email != "" && phoneNumber != "" && message != "") {
-        alert('Pesan Terkirim');
+        alert("Terimakasih! Pesan Terkirim")
+            // openPopup()
+    }
+}
+
+
+// text typing animation
+var string = "Eggan Nachson";
+var array = string.split("");
+var timer;
+
+function frameLooper() {
+    if (array.length > 0) {
+        document.getElementById("portof_name").innerHTML += array.shift()
+    } else {
+        clearTimeout(timer)
+    }
+    loopTimer = setTimeout('frameLooper()', 150)
+}
+
+frameLooper()
+
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+        document.getElementById("nav").style.padding = "20px";
+        document.getElementById("down").style.fontSize = "20px";
+    } else {
+        document.getElementById("nav").style.padding = "20px 0";
+        document.getElementById("down").style.fontSize = "10px";
     }
 }
